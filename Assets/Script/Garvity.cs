a9b432756574d18b9c4bbb673aaa4c5ea034b38c
Deadend002 <chirat.aiam@bumail.net>
2025-03-10 10:11:00 +0700

    Script_Update

--- Assets/Script/Garvity.cs
+++ Assets/Script/Garvity.cs
@@ -1,16 +1,26 @@
+using System;
 using UnityEngine;
 
 public class Garvity : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+   Rigidbody rb;
+    const float G = 0.00674f;
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+    private void FixedUpdate()
     {
         
     }
-
-    // Update is called once per frame
-    void Update()
+    void Attract(Garvity other)
     {
-        
+        Rigidbody rbOther = GetComponent<Rigidbody>();
+        Vector3 direction = rb.position - rbOther.position;
+        float distance = direction.magnitude;
+        if (distance == 0) { return; }
+        float forceMagnitude = G * ((rb.mass * rbOther.mass) / Mathf.Pow(distance, 2));
+        Vector3 force = forceMagnitude * direction.normalized;
     }
 }
+    
